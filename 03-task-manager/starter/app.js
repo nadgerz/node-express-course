064ed7355548f3bd0de363b5d8754b0a4eaba102
Stephen A. Ingram <stephem.a.ingram@gmail.com>
2024-09-19 16:51:11 +0200

    Basic Express App.

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,14 @@
 console.log('Task Manager App')
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Task Manager App')
+})
+
+const port = process.env.PORT || 4444
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
