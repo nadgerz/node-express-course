6bc19dbb4b01a24e8dfd461897f47eba6a5af28c
Stephen A. Ingram <stephem.a.ingram@gmail.com>
2024-09-19 18:18:25 +0200

    prettier. For old eyes.

--- 03-task-manager/starter/public/edit-task.js
+++ 03-task-manager/starter/public/edit-task.js
@@ -9,59 +9,59 @@ const id = new URLSearchParams(params).get('id')
 let tempName
 
 const showTask = async () => {
-  try {
-    const {
-      data: { task },
-    } = await axios.get(`/api/v1/tasks/${id}`)
-    const { _id: taskID, completed, name } = task
+    try {
+        const {
+            data: { task },
+        } = await axios.get(`/api/v1/tasks/${id}`)
+        const { _id: taskID, completed, name } = task
 
-    taskIDDOM.textContent = taskID
-    taskNameDOM.value = name
-    tempName = name
-    if (completed) {
-      taskCompletedDOM.checked = true
+        taskIDDOM.textContent = taskID
+        taskNameDOM.value = name
+        tempName = name
+        if (completed) {
+            taskCompletedDOM.checked = true
+        }
+    } catch (error) {
+        console.log(error)
     }
-  } catch (error) {
-    console.log(error)
-  }
 }
 
 showTask()
 
-editFormDOM.addEventListener('submit', async (e) => {
-  editBtnDOM.textContent = 'Loading...'
-  e.preventDefault()
-  try {
-    const taskName = taskNameDOM.value
-    const taskCompleted = taskCompletedDOM.checked
+editFormDOM.addEventListener('submit', async e => {
+    editBtnDOM.textContent = 'Loading...'
+    e.preventDefault()
+    try {
+        const taskName = taskNameDOM.value
+        const taskCompleted = taskCompletedDOM.checked
 
-    const {
-      data: { task },
-    } = await axios.patch(`/api/v1/tasks/${id}`, {
-      name: taskName,
-      completed: taskCompleted,
-    })
+        const {
+            data: { task },
+        } = await axios.patch(`/api/v1/tasks/${id}`, {
+            name: taskName,
+            completed: taskCompleted,
+        })
 
-    const { _id: taskID, completed, name } = task
+        const { _id: taskID, completed, name } = task
 
-    taskIDDOM.textContent = taskID
-    taskNameDOM.value = name
-    tempName = name
-    if (completed) {
-      taskCompletedDOM.checked = true
+        taskIDDOM.textContent = taskID
+        taskNameDOM.value = name
+        tempName = name
+        if (completed) {
+            taskCompletedDOM.checked = true
+        }
+        formAlertDOM.style.display = 'block'
+        formAlertDOM.textContent = `success, edited task`
+        formAlertDOM.classList.add('text-success')
+    } catch (error) {
+        console.error(error)
+        taskNameDOM.value = tempName
+        formAlertDOM.style.display = 'block'
+        formAlertDOM.innerHTML = `error, please try again`
     }
-    formAlertDOM.style.display = 'block'
-    formAlertDOM.textContent = `success, edited task`
-    formAlertDOM.classList.add('text-success')
-  } catch (error) {
-    console.error(error)
-    taskNameDOM.value = tempName
-    formAlertDOM.style.display = 'block'
-    formAlertDOM.innerHTML = `error, please try again`
-  }
-  editBtnDOM.textContent = 'Edit'
-  setTimeout(() => {
-    formAlertDOM.style.display = 'none'
-    formAlertDOM.classList.remove('text-success')
-  }, 3000)
+    editBtnDOM.textContent = 'Edit'
+    setTimeout(() => {
+        formAlertDOM.style.display = 'none'
+        formAlertDOM.classList.remove('text-success')
+    }, 3000)
 })
